265aaf42c6c92da3f72eafd000f53b280f598058
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-03-31 13:39:32 +0400

    add firebase SDK

---
+++ FireBaseRegister/LogInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogInViewController.swift
+//  FireBaseRegister
+//
+//  Created by Natia's Mac on 28/03/2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
