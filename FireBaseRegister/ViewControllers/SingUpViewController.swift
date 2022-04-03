db5c2f56d66d166e2f65d04325a7115eec19e840
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-04-03 15:09:07 +0400

    user was successfully created

--- FireBaseRegister/ViewControllers/SingUpViewController.swift
+++ FireBaseRegister/ViewControllers/SingUpViewController.swift
@@ -5,42 +5,43 @@
 //  Created by Natia's Mac on 31/03/2022.
 //
 
-//import UIKit
-//import Firebase
-//
-//class SingUpViewController: UIViewController {
-//    
-//    @IBOutlet weak var userName: UITextField!
-//    @IBOutlet weak var userEmail: UITextField!
-//    @IBOutlet weak var userPassword: UITextField!
-//    
-//    @IBOutlet weak var repeatPassword: UITextField!
-//    
-//    @IBOutlet weak var signUpBtn: UIButton!
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//    }
-//    
-//    @IBAction func createAccount(_ sender: Any) {
-//        
-//        let signUpManager = FireBaseAuthManager()
-//        if let email = userEmail.text, let password = userPassword.text {
-//            signUpManager.createUser(email: email, password: password){[weak self] (success) in
-//                guard let `self` = self else { return }
-//                var message: String = ""
-//                
-//                if success {
-//                    message = "User was sucessfully created"
-//                } else {
-//                    message = "There was an error"
-//                }
-//                let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-//                alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-//                self.present(alertController, animated: true)
-//              
-//            }
-//        }
-//    }
-//    
+import UIKit
+import Firebase
 
+class SingUpViewController: UIViewController {
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    
+    @IBOutlet weak var repeatPassword: UITextField!
+    
+    @IBOutlet weak var signUpBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func createAccount(_ sender: Any) {
+        
+        let signUpManager = FireBaseAuthManager()
+        if let email = userEmail.text, let password = userPassword.text {
+            signUpManager.createUser(email: email, password: password){[weak self] (success) in
+                guard let `self` = self else { return }
+                var message: String = ""
+                
+                if success {
+                    message = "User was sucessfully created"
+                } else {
+                    message = "There was an error"
+                }
+                let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alertController, animated: true)
+              
+            }
+        }
+    }
+    
+
+}
